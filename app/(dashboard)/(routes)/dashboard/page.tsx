81ee5e7504c705f523b3dd8f244774e6ac8e4c69
Greg Plumbly <greg.plumbly@gmail.com>
2023-09-10 22:58:56 +0100

    start dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+
+import { tools } from "@/constants";
+
 export default function Dashboardpage() {
   return (
     <div>
@@ -8,6 +15,23 @@ export default function Dashboardpage() {
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Chat with the smartest AI - Experience the power of AI
         </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
       </div>
     </div>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
   //   const isPro = await checkSubscription();
 
   return (
-    <div className="flex items-center p-4 bg-slate-100">
+    <div className="flex items-center p-4 ">
       {/* <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} /> */}
       <MobileSidebar />
       <div className="flex w-full justify-end">
