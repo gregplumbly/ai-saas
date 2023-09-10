44da7bccee4d7f62859eaad2edd91e165d61509d
Greg Plumbly <greg.plumbly@gmail.com>
2023-09-10 19:45:11 +0100

    add sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,14 @@
-
 export default function Dashboardpage() {
   return (
-    <h1>Dashboard protected</h1>
-  )
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+    </div>
+  );
 }
-
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage unprotected</div>
-  )
-}
+    <>
+      <div>LandingPage unprotected</div>
+      <Link href="/sign-up">
+        <Button variant="outline" className="rounded-full">
+          Sign up
+        </Button>
+      </Link>
+      <Link href="/sign-in">
+        <Button variant="outline" className="rounded-full">
+          Sign in
+        </Button>
+      </Link>
+    </>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
