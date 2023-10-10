d2eb9b555da82b6edf8174c2eff2fdbc643acaf8
Greg Plumbly <greg.plumbly@gmail.com>
2023-10-10 17:55:38 +0100

    start stripe integration.

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
     useEffect(() => {
-        Crisp.configure(process.env.CRISP_ID!);
+        Crisp.configure("aeeef02a-7f6e-49e8-8dbb-d76094f0c69b");
     }, []);
 
     return null;
